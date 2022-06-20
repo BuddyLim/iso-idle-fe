0872ef19d24cd59a63cc5478d2dbbcecd1533340
Buddy <buddy.tlimk@gmail.com>
2022-06-20 17:30:17 +0800

    [Add] Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Home from 'components/Home/index-Home';
+import React, { useEffect } from 'react';
+import 'styles/reset.css'
+import 'styles/App.css'
+import { io } from "socket.io-client";
 
 function App() {
+  useEffect(() =>{
+    const socket = io(process.env.REACT_APP_WS_BE_URL as string);
+    socket.emit("message")
+    return () =>{
+      socket.close()
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Home/>
     </div>
   );
 }
---
+++ src/components/Home/index-Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Home = () => {
+  return (<div>Home 2 Home</div>)
+}
+
+export default Home
